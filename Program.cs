5108719793f63fb49369c99531eee348a5d744f3
kalpesh marathe <1234kalpeshmj@gmail.com>
2025-04-09 11:24:48 +0530

    change approach from xpath to html Name selector

--- Program.cs
+++ Program.cs
@@ -1,52 +1,58 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System;
 
 
-class cloudQAFormAutomationTest {
+
+class cloudQAFormAutomationTest
+{
 
     public static void Main()
     {
-
+      
         IWebDriver driver = new ChromeDriver();
 
         try
         {
+            // Navigate to the CloudQA Automation Practice Form URL
             driver.Navigate().GoToUrl("https://app.cloudqa.io/home/AutomationPracticeForm");
+
+            // Wait for the page to load 
             Thread.Sleep(2000);
 
+            // Fill in the First Name field
+            var firstName = driver.FindElement(By.Name("First Name")); // Entering my first name
+            firstName.SendKeys("Kalpesh");
 
-            var firstNameInput = driver.FindElement(By.XPath("/html/body/div[1]/form/div[1]/div[2]/input"));
-            firstNameInput.SendKeys("Kalpesh");
+            // Fill in the Last Name field
+            var lastNameInput = driver.FindElement(By.Name("Last Name")); ;
+            lastNameInput.SendKeys("Marathe"); // Entering my last name
 
-            var lastNameInput = driver.FindElement(By.XPath("/html/body/div[1]/form/div[1]/div[3]/input"));
-            lastNameInput.SendKeys("Marathe");
 
-            var genderInput = driver.FindElement(By.XPath("/html/body/div[1]/form/div[1]/div[4]/div/input[1]"));
-            genderInput.Click();
+            // Enter Date of Birth
+            var DOBInput = driver.FindElement(By.Id("dob")); ;
+            DOBInput.SendKeys("2003-08-12"); // Format should be yyyy-mm-dd
 
-            var DOBInput = driver.FindElement(By.XPath("/html/body/div[1]/form/div[1]/div[5]/input"));
-            DOBInput.SendKeys("2003-08-12");
+            // Fill in Email address
+            var emailInput = driver.FindElement(By.Name("Email")); ;
+            emailInput.SendKeys("KalpeshMarathe@gmail.com"); // Typing email
 
-            var emailInput = driver.FindElement(By.XPath("/html/body/div[1]/form/div[1]/div[7]/input"));
-            emailInput.SendKeys("KalpeshMarathe@gmail.com");
+            // Click the Submit button
+            var submit = driver.FindElement(By.XPath("/html/body/div[1]/form/div[4]/button[1]")); ;
+            submit.Click(); // Submitting the form
 
-            var submit = driver.FindElement(By.XPath("/html/body/div[1]/form/div[4]/button[1]"));
-            submit.Click();
-            
         }
-        catch (Exception ex) {
-
-            Console.WriteLine("❌ Test failed: " + ex.Message);
-        }finally
+        catch (Exception ex)
         {
+            // Catch any errors that occur and print them
+            Console.WriteLine("Test failed: " + ex.Message);
+        }
+        finally
+        {
+            // Wait for 3 seconds before closing the browser
             Thread.Sleep(3000);
+
+            // Close the browser
             driver.Quit();
         }
-
     }
-
-
 }
-
-
